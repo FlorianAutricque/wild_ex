c93206d70d6b37b8fb4bd9ca8471417199ec11c7
Florian Autricque <fautricque@gmail.com>
2025-02-13 11:16:15 +0000

    card exo

--- project_cards/script.js
+++ project_cards/script.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll("nav a").forEach((link) => {
     link.addEventListener("click", function (event) {
+      if (this.target === "_blank") return;
+
       event.preventDefault();
       const iframe = document.getElementById("iframe");
       const newSrc = this.getAttribute("data-src");
 
       iframe.src = newSrc;
 
-      if (this.textContent === "Music") {
+      if (this.textContent.trim() === "Music") {
         setTimeout(() => {
           alert(
-            "Cette page n'est pas responsive 😊 Ciques sur ok pour continuer!"
+            "Cette page n'est pas responsive 😊 Cliques sur ok pour continuer"
           );
         }, 500);
       }
